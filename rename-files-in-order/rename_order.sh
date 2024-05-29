e1c0ef6b643471ea2d4b53424de93bcb467d570b
mattsalves <matt.git@skiff.com>
2024-05-29 05:38:19 -0400

    Add files via upload

---
+++ rename-files-in-order/rename_order.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#lol
+
+./rename_order.sh ~/Documentos/Dotfiles/Wallpaper 1
+
+                    
+
+#Error or Help
+if [ $# -ne 2 ] && [ $# -ne 1 ] 
+then
+    echo "Usage: rename.sh [PATH] [first_number] "
+    exit 1 
+#default begin number
+elif[ $# -e 1 ]
+
+    begin_number=0
+    path="$1"
+
+#2 args
+else
+    path="$1"
+    begin_number=$2
+fi
+
+main(){
+    #initilizing counter for use in name
+    counter=$begin_number
+    
+    for file in $path/*;do
+            
+        file_extension=${file##*.}
+        #format string of filename
+        new_name=$path/$(printf "%03i.%s" $counter $file_extension )
+
+        mv $file $new_name
+        #add 1 for counter    
+        let counter=$counter+1
+
+    done
+}
+
+main
